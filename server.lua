b6ff9d51727674dac6f68ed26d5d751bdd239cc1
Kõpskink <154380199+Kopskink@users.noreply.github.com>
2025-02-20 00:28:06 +0200

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,35 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+lib.addCommand('anotify', {
+    help = 'Saada admin teavitus soovitud isikule.',
+    restricted = 'group.admin'
+}, function(source, args, raw)
+    TriggerClientEvent('kps-anotify:client:PullUpInputDialog', source)
+end)
+
+RegisterNetEvent('kps-anotify:server:DisplayNotify')
+AddEventHandler('kps-anotify:server:DisplayNotify', function(targetid, desc, duration, title, pos, type)
+
+    local whole_server = QBCore.Functions.GetQBPlayers()
+
+    if targetid == 0 then
+        for _, player in pairs(whole_server) do
+            lib.notify(player.PlayerData.source, {
+                title = title,
+                description = desc,
+                duration = duration,
+                type = type,
+                position = pos,
+            })
+        end
+        return
+    end
+
+    lib.notify(targetid, {
+        title = title,
+        description = desc,
+        duration = duration,
+        type = type,
+        position = pos,
+    })
+end)
