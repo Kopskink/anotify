935aa120bf3ff4717829534551e896b51f1dbb24
Kõpskink <154380199+Kopskink@users.noreply.github.com>
2025-02-20 00:28:33 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'kopskink'
+description 'KPS-ANotify'
+version '1.0'
+
+client_scripts {
+	'client.lua',
+}
+server_script {
+    'server.lua',
+}
+
+shared_script {
+    '@ox_lib/init.lua',
+}
